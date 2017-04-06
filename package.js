1e1faeb11c133eb39de19363a846591e5836c2c1
Abigail Watson <awatson1978@gmail.com>
2017-04-06 00:16:15 -0500

    fhir-vault-server is optional

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-bundle',
-  version: '1.3.13',
+  version: '1.3.14',
   summary: 'HL7 FHIR Resource - Bundle',
   git: 'https://github.com/clinical-meteor/hl7-resource-bundle',
   documentation: 'README.md'
@@ -14,7 +14,6 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:hl7-resource-datatypes@3.0.0');
@@ -22,6 +21,10 @@ Package.onUse(function (api) {
   api.addFiles('lib/hl7-resource-bundle.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
+
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }  
 
   api.export('Bundle');
   api.export('Bundles');
