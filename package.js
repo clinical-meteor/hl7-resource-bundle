081804a249479cc392f686953c97c90e59b1f78d
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-01 11:49:16 -0500

    simpl-schema updates; hipaa-logger support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-bundle',
-  version: '1.3.14',
+  version: '1.4.0',
   summary: 'HL7 FHIR Resource - Bundle',
   git: 'https://github.com/clinical-meteor/hl7-resource-bundle',
   documentation: 'README.md'
@@ -11,12 +11,14 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.5.0');
+  api.use('ecmascript@0.9.0');
+  
   api.use('simple:json-routes@2.1.0');
 
-  api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:base-model@1.4.0');
+  
+  api.use('aldeed:collection2@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@4.0.0');
 
   api.addFiles('lib/hl7-resource-bundle.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
@@ -30,3 +32,8 @@ Package.onUse(function (api) {
   api.export('Bundles');
   api.export('BundleSchema');
 });
+
+
+Npm.depends({
+  "simpl-schema": "1.5.3"
+})
